a5cf43b1141eb3fdcff15c552cf7f8b0def883fa
sohandsone <932130168@qq.com>
2016-03-16 15:40:21 +0800

    测试......

---
+++ src/main/java/test_01/test_01/App.java
@@ -0,0 +1,13 @@
+package test_01.test_01;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/test_01/test_01/Maven1.java
@@ -0,0 +1,19 @@
+/**
+ *
+ * TODO
+ * TODO
+ *
+ * @author Administrator
+ * 
+ */
+package test_01.test_01;
+
+/**
+ * @author Administrator
+ *
+ */
+public class Maven1 {
+	void display(){
+		System.out.println( "maven 1" );
+	}
+}
